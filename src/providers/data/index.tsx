47493f3525135ff22f597897d2a93ddf16ea7ed2
ushantukey <ushantukey16@gmail.com>
2025-09-01 01:17:31 +0530

    Complete the auth

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,10 +1,17 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, {
+    GraphQLClient,
+    liveProvider as graphqlLiveProvider
+} from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient } from "graphql-ws";
 
+
+export const API_BASE_URL = 'https://api.crm.refine.dev'
 export const API_URL = 'https://api.crm.refine.dev'
+export const WS_URL = 'wss://api.crm.refine.dev/graphql'
 
-export const client = new  GraphQLClient(API_URL, {
-    fetch: (url: string, options: RequestInit)=>{
+export const client = new GraphQLClient(API_URL, {
+    fetch: (url: string, options: RequestInit) => {
         try {
             return fetchWrapper(url, options);
         } catch (error) {
@@ -12,3 +19,22 @@ export const client = new  GraphQLClient(API_URL, {
         }
     }
 })
+
+
+export const wsClient = typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+            const accessToken = localStorage.getItem("access_token");
+
+            return {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                }
+            }
+        }
+    })
+    : undefined
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined
